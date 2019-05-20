c0d64080c9dcc669e2a357de589d516ffc3b8abf
Gaytohub <tiamo39@vip.qq.com>
2019-05-20 22:54:43 +0800

    Update Mechine.java

--- src/CellMechine/Mechine.java
+++ src/CellMechine/Mechine.java
@@ -4,27 +4,38 @@ import java.awt.Color;
 
 public class Mechine {
 	private int Size;
-	
+	//private int Resource = 2000;
+
 	private Cell[][] mechine;
-	
 	
 	public Mechine(int size)
 	{
 		this.Size = size;
 		
 		mechine= new Cell[Size][Size];//初始化细胞数组
-		
+
+        //int usedRes = 0;
+        for (int count = 0; count < this.Size * this.Size / 4; count++){
+            int i = (int)(Math.random() * this.Size);
+            int j = (int)(Math.random() * this.Size);
+            int res = (int)(Math.random() * 50);
+            if (mechine[i][j] instanceof live){
+                count--;
+            }
+            else {
+                mechine[i][j] = new live(res, (int)(Math.random() * 50));
+            }
+        }
+
+
 		for(int i = 0; i < this.Size; i++)//随机生成活细胞和死细胞
 		{
 			for(int j = 0 ; j < this.Size; j++)
 			{
-				if(Math.random() < 0.2)
+				if(! (mechine[i][j] instanceof live))
 				{
-					mechine[i][j] = new live((int)(Math.random() * 50),(int)(Math.random() * 50));
-				}
-				else
 					mechine[i][j] = new Die();
-				
+				}
 			}
 		}
 	}
@@ -75,16 +86,16 @@ public class Mechine {
 
 					//判断某个细胞周围又空地
 					if(i > 0 && (mechine[i - 1][j] instanceof Die)){
-						born(mechine[i][j], mechine[i - 1][j]);
+						born(mechine[i][j], i - 1, j);
 					}
 					if(j > 0 && (mechine[i][j - 1] instanceof Die)){
-						born(mechine[i][j], mechine[i][j - 1]);
+						born(mechine[i][j], i ,j - 1);
 					}
 					if((i < (Size - 1) && mechine[i + 1][j] instanceof Die)){
-						born(mechine[i][j], mechine[i + 1][j]);
+						born(mechine[i][j], i + 1, j );
 					}
 					if(j < (Size - 1) && mechine[i][j + 1] instanceof Die){
-						born(mechine[i][j], mechine[i][j + 1]);
+						born(mechine[i][j], i, j + 1);
 					}
 
 				}
@@ -94,15 +105,18 @@ public class Mechine {
 	}
 
 	//第一个cell一定是live，第二个是die
-	private void born(Cell oldCell, Cell newCell)
+	private void born(Cell oldCell, int i, int j)
 	{
-		if(Math.random() < 0.15)	//这个几率要改
+		if(((live)oldCell).resource > 100)	//这个几率要改
 		{
 			/**
 			 * 在这个地方写生孩子的函数
 			 */
+            ((live)oldCell).resource = ((live)oldCell).resource / 2;
+            mechine[i][j] = new live(((live)oldCell).resource / 2, ((live)oldCell).skill/2);
 		}
 	}
+
 	//两个cell都是live
 	private void trade(Cell cellOne, Cell cellTwo){
 		if(Math.random() < 0.15)	//这个几率要改
